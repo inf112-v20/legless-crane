8e3add4ee7f0248310869eaa6eee4ffa7325902c
Sindre Larsen <sindrla92@gmail.com>
2020-02-04 15:49:45 +0100

    liten fiks

--- src/main/java/RoboRally/Renderer/PlayScreen.java
+++ src/main/java/RoboRally/Renderer/PlayScreen.java
@@ -5,22 +5,34 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class PlayScreen implements Screen {
     private TiledMap board;
+    private TiledMapTileLayer background, belts, holes, walls, spawn;
+
+
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
 
     @Override
     public void show() {
         TmxMapLoader loader = new TmxMapLoader();
-        board = loader.load("boards/board1.tmx");
-
+        board = loader.load("boards/board0.tmx"); // not mentioned in the libGDX / Tiled tutorial, unnecessary?
+        background = (TiledMapTileLayer) board.getLayers().get("Background");
+        belts = (TiledMapTileLayer) board.getLayers().get("Belts");
+        holes = (TiledMapTileLayer) board.getLayers().get("Holes");
+        walls = (TiledMapTileLayer) board.getLayers().get("Walls");
+        spawn = (TiledMapTileLayer) board.getLayers().get("Spawn");
         renderer = new OrthogonalTiledMapRenderer(board);
 
         camera = new OrthographicCamera();
+        camera.setToOrtho(false, 15,15);
+
+        camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0);
+        camera.update();
     }
 
     @Override
