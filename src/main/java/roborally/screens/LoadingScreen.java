f6d2c867e1fe37c77b7df1c7cd78c1bdd2a1e083
cej006 <43522317+cej006@users.noreply.github.com>
2020-02-22 21:24:28 +0100

    Create "loading screen" (first screen to be viewed)

--- src/main/java/roborally/screens/LoadingScreen.java
+++ src/main/java/roborally/screens/LoadingScreen.java
@@ -15,13 +15,45 @@ public class LoadingScreen implements Screen {
     private ShapeRenderer shapeRenderer;
     private float progress;
 
+    public LoadingScreen(final Renderer app){
+        this.app = app;
+        this.shapeRenderer = new ShapeRenderer();
+
+        queueAssets();
+    }
+
+    private void update(float f){
+        progress = MathUtils.lerp(progress, app.assets.getProgress(),.2f);
+
+        if (app.assets.update() && progress <= app.assets.getProgress()-.001f){       // Directs to game after all upload. (What is inside ququeAssets();)
+            app.setScreen(app.menuScreen);
+        }
+    }
+
     @Override
     public void show() {
-
+        System.out.println("Loading");
+        this.progress = 0f;
+        queueAssets();
     }
 
     @Override
     public void render(float v) {
+        Gdx.gl.glClearColor(25f, 25f, 25f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        update(v);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(32,app.camera.viewportHeight/2-8,app.camera.viewportWidth-64, 16);
+        shapeRenderer.setColor(Color.GREEN);
+        shapeRenderer.rect(32,app.camera.viewportHeight/2-8, progress* (app.camera.viewportWidth -64), 16);
+        shapeRenderer.end();
+
+        app.batch.begin();
+        app.font.draw(app.batch, "Screen is loading",20,20);
+        app.batch.end();
 
     }
 
@@ -47,6 +79,11 @@ public class LoadingScreen implements Screen {
 
     @Override
     public void dispose() {
+        shapeRenderer.dispose();
+    }
 
+    private void queueAssets(){
+            // Everything that has to be in order before next screen
+            app.assets.load("ui/uiskin.atlas", TextureAtlas.class);
     }
 }
