1f3cc1ce6ea53a9133af221ac7fc05c22c86fdf3
Sindre Larsen <sindrla92@gmail.com>
2020-02-27 19:40:32 +0100

    Fixed the bug with using assertTrue()

--- src/test/java/testing/tempTileObjectTests.java
+++ src/test/java/testing/tempTileObjectTests.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class tempTileObjectTests {
@@ -21,39 +20,41 @@ public class tempTileObjectTests {
 
     @Test
     public void boardCorrectlyReadsHoles() {
-        assertTrue(board.get(1,3).canKillPlayer());
+        assertEquals(true, board.get(1, 3).canKillPlayer());
     }
 
     @Test
     public void boardCorrectlyReadsWrenches() {
-        assertTrue(board.get(3,0).canRepair());
+        assertEquals(true, board.get(3, 0).canRepair());
     }
 
     @Test
-    public void boardCorrectlyReadsCogs() { assertTrue(board.get(2,4).canRotate()); }
+    public void boardCorrectlyReadsCogs() {
+        assertEquals(true, board.get(2, 4).canRotate()); }
 
     @Test
     public void boardCorrectlyReadsYellowBelts() {
-        assertTrue(board.get(0,2).canMovePlayer());
+        assertEquals(true, board.get(0, 2).canMovePlayer());
         assertEquals(board.get(0,2).getMovementSpeed(),1);
     }
 
     @Test
     public void boardCorrectlyReadsBlueBelts() {
-        assertTrue(board.get(4,4).canMovePlayer());
+        assertEquals(true, board.get(4, 4).canMovePlayer());
         assertEquals(board.get(4,4).getMovementSpeed(),2);
     }
 
     @Test
-    public void boardCorrectlyReadsSpawnPoints() { assertTrue(board.get(1,0).isSpawner()); }
+    public void boardCorrectlyReadsSpawnPoints() {
+        assertEquals(true, board.get(1, 0).isSpawner()); }
 
     @Test
     public void boardCorrectlyReadsFlags() {
-        assertTrue(board.get(3,1).isFlag());
+        assertEquals(true, board.get(3, 1).isFlag());
     }
 
     @Test
     public void boardCorrectlyReadsWalls() {
-        assertTrue(board.get(1,4).canBlockMovement());
+        assertEquals(true, board.get(1, 4).canBlockMovement());
     }
 }
