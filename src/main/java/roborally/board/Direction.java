b5fd2af491e32d92697fb0b1cb3d8beac615eb55
Sindre <sindrla92@gmail.com>
2020-03-23 16:42:18 +0100

    Added documentation to Direction.java
    issue #87

--- src/main/java/roborally/board/Direction.java
+++ src/main/java/roborally/board/Direction.java
@@ -2,10 +2,20 @@ package roborally.board;
 
 import java.util.ArrayList;
 
+/**
+ * Enum containing possible directions a player or other elements could be facing or operate in.
+ * Since diagonal movement so far is not a factor, this should work very well for the time being.
+ */
 public enum Direction {
     NORTH, EAST, SOUTH, WEST;
 
-
+    /**
+     * When used, this method returns the opposite direction of the one it's used on.
+     *
+     * Direction.EAST.opposite() would give Direction.WEST etc.
+     *
+     * @return the opposite direction
+     */
     public Direction opposite() {
         switch (this) {
             case EAST: return WEST;
@@ -16,7 +26,15 @@ public enum Direction {
         }
     }
 
-    public Direction rotate(int rotation, Direction currentDirection) {
+    /**
+     * rotate() allows us to get the direction after rotating clockwise or counter-clockwise the direction and distance
+     * determined by the parameter.
+     *
+     * @param rotation a positive or negative integer which determines which direction and how much to rotate
+     *                 negative value -> counter-clockwise, positive value -> clockwise
+     * @return the direction post-rotation
+     */
+    public Direction rotate(int rotation) {
         // allows us to accept a negative or positive int for how much and how little it should rotate
         // negative value is counter-clockwise, positive is clockwise
         ArrayList<Direction> directions = new ArrayList<>();
@@ -24,7 +42,7 @@ public enum Direction {
         directions.add(EAST);
         directions.add(SOUTH);
         directions.add(WEST);
-        int index = directions.indexOf(currentDirection);
+        int index = directions.indexOf(this);
 
         if ((rotation + index) % directions.size() < 0)
             return directions.get(((rotation + index) % directions.size()) + directions.size());
