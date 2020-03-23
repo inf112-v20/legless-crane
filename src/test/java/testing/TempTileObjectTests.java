2b4d91bf342b52438523902c753e38994839ed0b
Sindre <sindrla92@gmail.com>
2020-03-23 18:39:28 +0100

    Renaming some methods for better readability in Tile.java

--- src/test/java/testing/TempTileObjectTests.java
+++ src/test/java/testing/TempTileObjectTests.java
@@ -16,47 +16,47 @@ public class TempTileObjectTests {
     private final Board board;
 
     public TempTileObjectTests() {
-        board = new Board(new File("src/test/testingResources/testBoard0.tmx"));
+        board = new Board("src/test/testingResources/testBoard0.tmx");
     }
 
     @Test
     public void boardCorrectlyReadsHoles() {
-        assertEquals(true, board.get(new Vector2(1,3)).canKillPlayer());
+        assertEquals(true, board.getTile(new Vector2(1,3)).isHole());
 
     }
 
     @Test
     public void boardCorrectlyReadsWrenches() {
-        assertEquals(true, board.get(new Vector2(3,0)).canRepair());
+        assertEquals(true, board.getTile(new Vector2(3,0)).isWrench());
     }
 
     @Test
     public void boardCorrectlyReadsCogs() {
-        assertEquals(true, board.get(new Vector2(2,4)).canRotate()); }
+        assertEquals(true, board.getTile(new Vector2(2,4)).isCog()); }
 
     @Test
     public void boardCorrectlyReadsYellowBelts() {
-        assertEquals(true, board.get(new Vector2(0,2)).canMovePlayer());
-        assertEquals(board.get(new Vector2(0,2)).getMovementSpeed(),1);
+        assertEquals(true, board.getTile(new Vector2(0,2)).isBelt());
+        assertEquals(board.getTile(new Vector2(0,2)).getMovementSpeed(),1);
     }
 
     @Test
     public void boardCorrectlyReadsBlueBelts() {
-        assertEquals(true, board.get(new Vector2(4,4)).canMovePlayer());
-        assertEquals(board.get(new Vector2(4,4)).getMovementSpeed(),2);
+        assertEquals(true, board.getTile(new Vector2(4,4)).isBelt());
+        assertEquals(board.getTile(new Vector2(4,4)).getMovementSpeed(),2);
     }
 
     @Test
     public void boardCorrectlyReadsSpawnPoints() {
-        assertEquals(true, board.get(new Vector2(1,0)).isSpawner()); }
+        assertEquals(true, board.getTile(new Vector2(1,0)).isSpawner()); }
 
     @Test
     public void boardCorrectlyReadsFlags() {
-        assertEquals(true, board.get(new Vector2(3,1)).isFlag());
+        assertEquals(true, board.getTile(new Vector2(3,1)).isFlag());
     }
 
     @Test
     public void boardCorrectlyReadsWalls() {
-        assertEquals(true, board.get(new Vector2(1,4)).canBlockMovement());
+        assertEquals(true, board.getTile(new Vector2(1,4)).canBlockMovement());
     }
 }
