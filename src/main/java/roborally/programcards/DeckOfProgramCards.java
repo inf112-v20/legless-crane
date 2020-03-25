bcf1a60c53c0d5f28a11831d5956079cf4bcea46
KjerstiDA <kjerstiaslesen@gmail.com>
2020-03-26 00:19:24 +0100

    added priority to programcards

--- src/main/java/roborally/programcards/DeckOfProgramCards.java
+++ src/main/java/roborally/programcards/DeckOfProgramCards.java
@@ -6,27 +6,27 @@ public class DeckOfProgramCards {
     private ArrayList<ProgramCard> deckOfProgramCards;
 
     public DeckOfProgramCards() {
-        deckOfProgramCards = new ArrayList<ProgramCard>();
+        deckOfProgramCards = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
-            deckOfProgramCards.add(new ProgramCard("back"));             // Move backwards
+            deckOfProgramCards.add(new ProgramCard("back", 430 + (i * 10)));             // Move backwards
         }
         for (int i = 0; i < 6; i++) {
-            deckOfProgramCards.add(new ProgramCard("u"));                // U-turn
+            deckOfProgramCards.add(new ProgramCard("u", 10 + (i * 10)));                // U-turn
         }
         for (int i = 0; i < 18; i++) {
-            deckOfProgramCards.add(new ProgramCard("rotateright"));      // Rotate right
+            deckOfProgramCards.add(new ProgramCard("rotateright", 80 + (i * 20)));      // Rotate right
         }
         for (int i = 0; i < 18; i++) {
-            deckOfProgramCards.add(new ProgramCard("rotateleft"));      // Rotate left
+            deckOfProgramCards.add(new ProgramCard("rotateleft", 70 + (i * 20)));      // Rotate left
         }
         for (int i = 0; i < 18; i++) {
-            deckOfProgramCards.add(new ProgramCard("1"));               // Move forward 1
+            deckOfProgramCards.add(new ProgramCard("1", 490 + (i * 10)));               // Move forward 1
         }
         for (int i = 0; i < 12; i++) {
-            deckOfProgramCards.add(new ProgramCard("2"));               // Move forward 2
+            deckOfProgramCards.add(new ProgramCard("2", 670 + (i * 10)));               // Move forward 2
         }
         for (int i = 0; i < 6; i++) {
-            deckOfProgramCards.add(new ProgramCard("3"));               // Move forward 3
+            deckOfProgramCards.add(new ProgramCard("3", 790 + (i * 10)));               // Move forward 3
         }
     }
 
--- src/main/java/roborally/programcards/ProgramCard.java
+++ src/main/java/roborally/programcards/ProgramCard.java
@@ -4,13 +4,16 @@ import roborally.ui.BaseActor;
 
 public class ProgramCard extends BaseActor {
     private String movement;
+    private int priority;
     // TODO: add value? (make program cards have priority)
 
-    public ProgramCard(String movement) {
+    public ProgramCard(String movement, int priority) {
         this.movement = movement;
+        this.priority = priority;
     }
-
     public String getMovement() {
         return movement;
     }
+
+    public int getPriority() {return priority;}
 }
