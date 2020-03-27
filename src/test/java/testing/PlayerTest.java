e5829f75e2141485fe2d4a7fb3606c33fc45caed
KjerstiDA <kjerstiaslesen@gmail.com>
2020-03-27 12:02:16 +0100

    more testing of player

--- src/test/java/testing/PlayerTest.java
+++ src/test/java/testing/PlayerTest.java
@@ -7,13 +7,16 @@ import roborally.board.Board;
 import roborally.board.Direction;
 import roborally.gamelogic.GameLogic;
 import roborally.gamelogic.Player;
+import static org.mockito.Mockito.mock;
 
 /**
  * Testing for correct initialisation of a player. This includes position, health etc.
  */
 public class PlayerTest {
-    private Player player = new Player(1, new Vector2(1,3), null);
+    private GameLogic gameLogic = mock(GameLogic.class);
+    private Vector2 startpos = new Vector2(1,1);
     private Board board = new Board("boards/testBoard0.tmx");
+    private Player player = new Player(0, startpos, gameLogic);
 
     @Test
     public void initialHealth(){
@@ -27,7 +30,7 @@ public class PlayerTest {
 
     @Test
     public void initialPosition(){
-        Assertions.assertEquals(new Vector2(1, 3), player.getPosition());
+        Assertions.assertEquals(startpos, player.getPosition());
     }
 
     @Test
@@ -37,9 +40,9 @@ public class PlayerTest {
 
     @Test
     public void changeOfPlayerPosition(){
-        player.setPosition(new Vector2(1, 1));
+        player.setPosition(new Vector2(1, 0));
         Assertions.assertNotEquals(player.getBackupPoint(), player.getPosition());
-        Assertions.assertEquals(new Vector2(1, 1), player.getPosition());
+        Assertions.assertEquals(new Vector2(1, 0), player.getPosition());
     }
 
     @Test
@@ -55,7 +58,22 @@ public class PlayerTest {
     }
 
     @Test
-    public void checkDamagePlayerTakesFromHole(){
-        Assertions.assertEquals(-10, board.getTile(new Vector2(1,3)).getHealthChange());
+    public void playerTakesDamageAndLosesALife(){
+        player.updateHealth(-10);
+        Assertions.assertEquals(2, player.getLives());
+    }
+
+    @Test
+    public void checkDamagePlayerTakesFromHoleIsTen(){
+        int damage = board.getTile(new Vector2(1,3)).getHealthChange();
+        Assertions.assertEquals(-10, damage);
+    }
+
+    @Test
+    public void checkForCorrectRespawnPosition(){
+        int damage = board.getTile(new Vector2(1, 3)).getHealthChange();
+        Assertions.assertEquals(-10, damage);
+        player.updateHealth(damage);
+        Assertions.assertEquals(startpos, player.getPosition());
     }
 }
