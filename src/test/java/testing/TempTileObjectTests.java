03cbfc10c75c76be3cbc3ba6f176f299679844e7
Sindre <sindrla92@gmail.com>
2020-04-02 22:48:33 +0200

    Unsure if this is updating the pull request, testing.

--- src/test/java/testing/TempTileObjectTests.java
+++ src/test/java/testing/TempTileObjectTests.java
@@ -5,7 +5,6 @@ import roborally.board.Board;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TempTileObjectTests {
