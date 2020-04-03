b6aeba93fd062e79b430805c4a7bec39d8c81395
KjerstiDA <kjerstiaslesen@gmail.com>
2020-04-03 13:40:23 +0200

    Headless application runner with mockito

---
+++ src/test/java/testing/TestGDXRunner.java
@@ -0,0 +1,54 @@
+package testing;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import org.mockito.Mockito;
+
+/**
+ * Runs a headless test of an Application
+ * Thus all methods here are empty
+ */
+public class TestGDXRunner implements BeforeAllCallback {
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws ExceptionInInitializerError {
+        new HeadlessApplication(new ApplicationListener() {
+            @Override
+            public void create() {
+
+            }
+
+            @Override
+            public void resize(int i, int i1) {
+
+            }
+
+            @Override
+            public void render() {
+
+            }
+
+            @Override
+            public void pause() {
+
+            }
+
+            @Override
+            public void resume() {
+
+            }
+
+            @Override
+            public void dispose() {
+
+            }
+        });
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+    }
+}
