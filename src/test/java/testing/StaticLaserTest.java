a2197b73cf449685c8b8412cfd662cb6c34d5f20
cej006 <43522317+cej006@users.noreply.github.com>
2020-05-08 10:09:42 +0200

    FlagTest and StaticLaserTest

--- src/test/java/testing/StaticLaserTest.java
+++ src/test/java/testing/StaticLaserTest.java
@@ -1,4 +1,36 @@
 package testing;
 
+import com.badlogic.gdx.math.Vector2;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import roborally.board.Board;
+import roborally.gamelogic.GameLogic;
+import roborally.gamelogic.Player;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 public class StaticLaserTest {
+        private static Board board;
+        private static Player player;
+        private static GameLogic gameLogic;
+
+        @BeforeAll
+        static void setup() {
+            board = new Board("boards/Risky_Exchange.tmx");
+            gameLogic = mock(GameLogic.class);
+            player = new Player(0, new Vector2(0, 0), gameLogic);
+        }
+
+        @Test
+        public void boardCorrectlyReadsStaticLaser() {
+        assertEquals(true, board.getTile(new Vector2(10, 14)).isLaser());
+        }
+
+        @Test
+        public void playerShouldLoseOneHealthPoint() {
+            player.setPosition(new Vector2(10, 14));
+            player.updateHealth(board.getTile(player.getPosition()).getHealthChange());
+            assertEquals(8, player.getHealth());
+        }
 }
